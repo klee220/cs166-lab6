3e2b896651c11f25a51274967b48bbdde9f48618
Karina Lee <klee220@wch133-22.cs.ucr.edu>
2021-02-25 05:30:11 -0800

    finished part 1

---
+++ lab6.1.sql
@@ -0,0 +1,30 @@
+SELECT COUNT(c.sid)
+FROM catalog c
+GROUP BY c.sid;
+
+SELECT COUNT(c.sid)
+FROM catalog c
+GROUP BY c.sid HAVING count(c.sid)>2;
+
+(SELECT s.sname, COUNT(c.sid)
+FROM suppliers s, catalog c, parts p
+WHERE s.sid = c.sid AND c.pid = p.pid AND p.color = 'Green'
+GROUP BY s.sname)
+EXCEPT
+(SELECT s.sname, COUNT(c.sid)
+FROM suppliers s, catalog c, parts p
+WHERE s.sid = c.sid AND c.pid = p.pid AND p.color != 'Green'
+GROUP BY s.sname);
+
+SELECT k.sname, MAX(c1.cost)
+FROM suppliers k, catalog c1
+WHERE c1.sid = k.sid AND k.sname IN ((SELECT s.sname
+FROM suppliers s, catalog c, parts p
+WHERE s.sid = c.sid AND c.pid = p.pid AND p.color = 'Green' 
+GROUP BY s.sname)
+INTERSECT
+(SELECT s.sname
+FROM suppliers s, catalog c, parts p
+WHERE s.sid = c.sid AND c.pid = p.pid AND p.color = 'Red' 
+GROUP BY s.sname))
+GROUP BY k.sname;
